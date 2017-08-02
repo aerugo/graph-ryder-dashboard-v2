c88b9640afdef79998fe24930cbfea21c9127c78
nferon <norbert.feron@etu.u-bordeaux.fr>
2017-08-02 14:47:07 +0200

    sigma demo mode with random graph for home page

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# graph-ryder-dashboard
+# graph-ryder-dashboard 2.0
 
 This project was generated with the [Angular Full-Stack Generator](https://github.com/DaftMonk/generator-angular-fullstack) version 4.2.2.
 
--- client/components/sigma/sigma.directive.ts
+++ client/components/sigma/sigma.directive.ts
@@ -5,43 +5,66 @@ require('script!linkurious/dist/plugins.min.js');
 
 // todo : https://github.com/Linkurious/linkurious.js/wiki/How-to-integrate-with-Angular.js
 export default angular.module('graphRyderDashboardApp.sigma', [])
-  .directive('sigma', function() {
-    let divId = 'sigmjs-dir-container-'+Math.floor((Math.random() * 999999999999))+'-'+Math.floor((Math.random() * 999999999999))+'-'+Math.floor((Math.random() * 999999999999));
+  .directive('sigma', function($compile) {
     return {
       restrict: 'E',
-      template: '<div id="'+divId+'" class="sigma-instance" style="width: 100%;height: 90vh;"></div>',
       scope: {
         //@ reads the attribute value, = provides two-way binding, & works with functions
         graph: '=',
-        info: '='
+        info: '=',
+        instanceId: '@',
+        instanceClass: '@',
+        settings: '='
       },
       link: function (scope, element, attrs) {
-        // Let's first initialize sigma:
+        /****** Inject the template *****/
+
+        $compile(element.contents())(scope);
+        element.html('<div id="'+ scope.instanceId +'" class="'+ scope.instanceClass +'" ></div>');
+
+        /****** Settings *********/
+        let settings = { // default settings
+          labelThreshold: 7,
+          dragNodeStickiness: 0.01,
+          nodeBorderSize: 2,
+          defaultNodeBorderColor: '#000',
+          enableEdgeHovering: true,
+          drawEdgeLabels: false,
+          animationsTime: 1500,
+          zoomDef: 1.1,
+          centerOnLoad: true
+        };
+        if(scope.settings)
+          Object.keys(scope.settings).forEach(function(key) { settings[key] = scope.settings[key]; });
+
+        /****** Initialize *******/
         let s = new sigma({
           renderer: {
-            container: divId,
+            container: element[0].firstChild,
             type: 'canvas'
           },
-          settings: {
-            defaultNodeColor: '#ec5148',
-            labelThreshold: 10,
-            dragNodeStickiness: 0.01,
-            nodeBorderSize: 2,
-            defaultNodeBorderColor: '#000',
-            enableEdgeHovering: true,
-            edgeHoverHighlightNodes: 'circle',
-          }
+          settings: settings
         });
+        // demo mode
+        if(settings.demo) {
+          sigma.layouts.fruchtermanReingold.configure(s, {
+            iterations: 500,
+            easing: 'quadraticInOut',
+            duration: 1500
+          });
+        }
         // console.log(plugins_locate);
         scope.$watch('graph', function() {
           if(scope.graph) {
             s.graph.clear();
             s.graph.read(scope.graph);
             s.refresh();
+            if(settings.demo)
+              sigma.layouts.fruchtermanReingold.start(s);
           }
         });
 
-
+        /**** Tools *****/
         let dragListener = sigma.plugins.dragNodes(s, s.renderers[0]);
         let lasso = new sigma.plugins.lasso(s, s.renderers[0], {
           'strokeStyle': 'black',
@@ -52,19 +75,23 @@ export default angular.module('graphRyderDashboardApp.sigma', [])
         });
         // lasso.activate();
 
+
+
         /**** Events ****/
         s.bind('clickNode clickEdge hovers', function(e){
-          scope.info = "x: " + e.data.captor.x + " y: " + e.data.captor.y; //todo: get rapid info on element
-          scope.$apply();
+          if(scope.info) {
+            scope.info = "x: " + e.data.captor.x + " y: " + e.data.captor.y; //todo: get rapid info on element
+            scope.$apply();
+          }
         });
 
         lasso.bind('selectedNodes', function(e){;
           console.log(e);
         });
 
-        element.on('$destroy', function() {
-          s.graph.clear();
-        });
+          // element.on('$destroy', function() {
+          //   s.graph.clear();
+          // });
       }
     };
   })
